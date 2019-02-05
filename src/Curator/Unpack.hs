qxjit/stack

Curator unpack and proper handling of expected test/haddock failures

subs/curator/src/Curator/Unpack.hs
@@ -24,16 +24,23 @@ unpackSnapshot
   -> RIO env ()
 unpackSnapshot cons snap root = do
   unpacked <- parseRelDir "unpacked"
-  (suffixes, flags, skipTest, skipBench, skipHaddock) <- fmap fold $ for (snapshotPackages snap) $ \sp -> do
+  (suffixes, flags, (skipTest, expectTestFailure), skipBench,
+   (skipHaddock, expectHaddockFailure)) <- fmap fold $ for (snapshotPackages snap) $ \sp -> do
     let pl = spLocation sp
     TreeKey (BlobKey sha _size) <- getPackageLocationTreeKey pl
     PackageIdentifier name version <- getPackageLocationIdent pl
-    pc <-
-      case Map.lookup name $ consPackages cons of
-        Nothing -> error $ "Package not found in constraints: " ++ packageNameString name
-        Just pc -> pure pc
-    unless (pcFlags pc == spFlags sp) $ error "mismatched flags!"
-    if pcSkipBuild pc
+    let (flags, skipBuild, test, bench, haddock) =
+          case Map.lookup name $ consPackages cons of
+            Nothing ->
+              (mempty, False, CAExpectSuccess, CAExpectSuccess, CAExpectSuccess)
+            Just pc ->
+              (pcFlags pc, pcSkipBuild pc, pcTests pc, pcBenchmarks pc, pcHaddock pc)
+    unless (flags == spFlags sp) $ error $ unlines
+      [ "mismatched flags for " ++ show pl
+      , " snapshot: " ++ show (spFlags sp)
+      , " constraints: " ++ show flags
+      ]
+    if skipBuild
       then pure mempty
       else do
         let suffixBuilder =
@@ -55,16 +62,19 @@ unpackSnapshot cons snap root = do
           renameDir destTmp dest
         pure
           ( Set.singleton suffix
-          , if Map.null (pcFlags pc) then Map.empty else Map.singleton name (pcFlags pc)
-          , case pcTests pc of
+          , if Map.null flags then Map.empty else Map.singleton name flags
+          , case test of
               CAExpectSuccess -> mempty
-              _ -> Set.singleton name -- FIXME this and others, want to differentiate skip and expect failure
-          , case pcBenchmarks pc of
+              CAExpectFailure -> (mempty, Set.singleton name)
+              CASkip ->  (Set.singleton name, mempty)
+          , case bench of
+              CASkip -> Set.singleton name
+              _ -> mempty -- FIXME maybe we want to differentiate skip and expect failure but
+                          -- we don't run benchmarks, only compile them
+          , case haddock of
               CAExpectSuccess -> mempty
-              _ -> Set.singleton name
-          , case pcHaddock pc of
-              CAExpectSuccess -> mempty
-              _ -> Set.singleton name
+              CAExpectFailure -> (mempty, Set.singleton name)
+              CASkip -> (Set.singleton name, mempty)
           )
   stackYaml <- parseRelFile "stack.yaml"
   let stackYamlFP = toFilePath $ root </> stackYaml
@@ -74,7 +84,9 @@ unpackSnapshot cons snap root = do
     , "flags" .= fmap toCabalStringMap (toCabalStringMap flags)
     , "curator" .= object
         [ "skip-test" .= Set.map CabalString skipTest
+        , "expect-test-failure" .= Set.map CabalString expectTestFailure
         , "skip-bench" .= Set.map CabalString skipBench
         , "skip-haddock" .= Set.map CabalString skipHaddock
+        , "expect-haddock-failure" .= Set.map CabalString expectHaddockFailure
         ]
     ]

commercialhaskell/curator

src/Curator/Unpack.hs
@@ -24,16 +24,23 @@ unpackSnapshot
   -> RIO env ()
 unpackSnapshot cons snap root = do
   unpacked <- parseRelDir "unpacked"
-  (suffixes, flags, skipTest, skipBench, skipHaddock) <- fmap fold $ for (rsPackages snap) $ \sp -> do
+  (suffixes, flags, (skipTest, expectTestFailure), skipBench,
+   (skipHaddock, expectHaddockFailure)) <- fmap fold $ for (rsPackages snap) $ \sp -> do
     let pl = rspLocation sp
     TreeKey (BlobKey sha _size) <- getRawPackageLocationTreeKey pl
     PackageIdentifier name version <- getRawPackageLocationIdent pl
-    pc <-
-      case Map.lookup name $ consPackages cons of
-        Nothing -> error $ "Package not found in constraints: " ++ packageNameString name
-        Just pc -> pure pc
-    unless (pcFlags pc == rspFlags sp) $ error "mismatched flags!"
-    if pcSkipBuild pc
+    let (flags, skipBuild, test, bench, haddock) =
+          case Map.lookup name $ consPackages cons of
+            Nothing ->
+              (mempty, False, CAExpectSuccess, CAExpectSuccess, CAExpectSuccess)
+            Just pc ->
+              (pcFlags pc, pcSkipBuild pc, pcTests pc, pcBenchmarks pc, pcHaddock pc)
+    unless (flags == rspFlags sp) $ error $ unlines
+      [ "mismatched flags for " ++ show pl
+      , " snapshot: " ++ show (rspFlags sp)
+      , " constraints: " ++ show flags
+      ]
+    if skipBuild
       then pure mempty
       else do
         let suffixBuilder =
@@ -55,16 +62,19 @@ unpackSnapshot cons snap root = do
           renameDir destTmp dest
         pure
           ( Set.singleton suffix
-          , if Map.null (pcFlags pc) then Map.empty else Map.singleton name (pcFlags pc)
-          , case pcTests pc of
+          , if Map.null flags then Map.empty else Map.singleton name flags
+          , case test of
               CAExpectSuccess -> mempty
-              _ -> Set.singleton name -- FIXME this and others, want to differentiate skip and expect failure
-          , case pcBenchmarks pc of
+              CAExpectFailure -> (mempty, Set.singleton name)
+              CASkip ->  (Set.singleton name, mempty)
+          , case bench of
+              CASkip -> Set.singleton name
+              _ -> mempty -- FIXME maybe we want to differentiate skip and expect failure but
+                          -- we don't run benchmarks, only compile them
+          , case haddock of
               CAExpectSuccess -> mempty
-              _ -> Set.singleton name
-          , case pcHaddock pc of
-              CAExpectSuccess -> mempty
-              _ -> Set.singleton name
+              CAExpectFailure -> (mempty, Set.singleton name)
+              CASkip -> (Set.singleton name, mempty)
           )
   stackYaml <- parseRelFile "stack.yaml"
   let stackYamlFP = toFilePath $ root </> stackYaml
@@ -74,7 +84,9 @@ unpackSnapshot cons snap root = do
     , "flags" .= fmap toCabalStringMap (toCabalStringMap flags)
     , "curator" .= object
         [ "skip-test" .= Set.map CabalString skipTest
+        , "expect-test-failure" .= Set.map CabalString expectTestFailure
         , "skip-bench" .= Set.map CabalString skipBench
         , "skip-haddock" .= Set.map CabalString skipHaddock
+        , "expect-haddock-failure" .= Set.map CabalString expectHaddockFailure
         ]
     ]