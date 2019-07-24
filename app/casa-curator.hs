a3515c89fe260283c9510eaf03546d736f48db15
Chris Done <chrisdone@gmail.com>
2019-07-24 11:23:18 +0100

    Add a populate command

--- app/casa-curator.hs
+++ app/casa-curator.hs
@@ -5,17 +5,21 @@
 
 module Main where
 
-import Casa.Client
-import Control.Lens.TH
-import Control.Monad.Trans.Resource
-import Data.Conduit
-import Options.Applicative
-import Options.Applicative.Simple
-import Pantry
-import Pantry.Internal.Stackage
-import RIO
-import RIO.Orphans
-import System.Environment
+import           Casa.Client
+import           Control.Lens.TH
+import           Control.Monad.Trans.Resource
+import           Data.Conduit
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           Options.Applicative
+import           Options.Applicative.Simple
+import           Pantry
+import           Pantry.Internal.Stackage
+import           RIO
+import           RIO.Orphans
+import           System.Environment
 
 data CasaPush =
   CasaPush
@@ -40,6 +44,21 @@ pushConfigParser =
   PushConfig <$>
   strOption (long "push-url" <> metavar "URL" <> help "Casa push URL")
 
+data PopulateConfig =
+  PopulateConfig
+    { populateConfigSnapshot :: Unresolved RawSnapshotLocation
+    }
+
+-- | Command-line config.
+populateConfigParser :: Parser PopulateConfig
+populateConfigParser =
+  PopulateConfig <$>
+  fmap
+    (parseRawSnapshotLocation . T.pack)
+    (strOption
+       (long "snapshot" <> metavar "SNAPSHOT" <>
+        help "Snapshot in usual Stack format (lts-1.1, nightly-...)"))
+
 -- | Main entry point.
 main :: IO ()
 main = do
@@ -50,12 +69,50 @@ main = do
       "casa-curator"
       (pure ())
       (do addCommand "push" "Push all blobs" pushCommand pushConfigParser
+          addCommand "status" "Give some stats" (const statusCommand) (pure ())
           addCommand
             "populate"
             "Populate the pantry database"
-            (const (pure ()))
-            (pure ()))
+            populateCommand
+            populateConfigParser)
   runCmd
+
+statusCommand :: IO ()
+statusCommand =
+  runPantryApp
+    (do pantryApp <- ask
+        storage <- fmap (pcStorage . view pantryConfigL) ask
+        withResourceMap
+          (\resourceMap ->
+             runRIO
+               (CasaPush
+                  { _casaPushResourceMap = resourceMap
+                  , _casaPushPantry = pantryApp
+                  })
+               (withStorage_
+                  storage
+                  (do count <- allBlobsCount
+                      lift (logInfo ("Blobs in database: " <> display count))))))
+
+populateCommand :: MonadIO m => PopulateConfig -> m ()
+populateCommand populateConfig =
+  runPantryApp
+    (do let unresoledRawSnapshotLocation = populateConfigSnapshot populateConfig
+        rawSnapshotLocation <- resolvePaths Nothing unresoledRawSnapshotLocation
+        snapshotLocation <- completeSnapshotLocation rawSnapshotLocation
+        logSticky "Loading snapshot ..."
+        rawSnapshot <- loadSnapshot snapshotLocation
+        logStickyDone "Loaded snapshot."
+        let total = length (rsPackages rawSnapshot)
+        for_
+          (zip
+             [0 :: Int ..]
+             (map rspLocation (M.elems (rsPackages rawSnapshot))))
+          (\(i, rawPackageLocationImmutable) -> do
+             logSticky
+               ("Loading package: " <> display i <> "/" <> display total <> ": " <>
+                display rawPackageLocationImmutable)
+             loadPackageRaw rawPackageLocationImmutable))
 
 -- | Start pushing.
 pushCommand :: MonadIO m => PushConfig -> m ()
