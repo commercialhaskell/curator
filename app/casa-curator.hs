79b17f551db2dac84f97f0a798e383cdc3a98650
Chris Done <chrisdone@gmail.com>
2019-07-24 14:28:01 +0100

    Implement downloadAllSnapshotTextNames

--- app/casa-curator.hs
+++ app/casa-curator.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -8,17 +10,26 @@ module Main where
 import           Casa.Client
 import           Control.Lens.TH
 import           Control.Monad.Trans.Resource
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Parser
+import qualified Data.Aeson.Types as Aeson
 import           Data.Conduit
+import           Data.Generics
+import           Data.List
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import           Data.Set (Set)
+import qualified Data.Set as Set
 import           Data.Text (Text)
 import qualified Data.Text as T
+import           Network.HTTP.Simple
 import           Options.Applicative
 import           Options.Applicative.Simple
 import           Pantry
 import           Pantry.Internal.Stackage
 import           RIO
 import           RIO.Orphans
+import           RIO.Process
 import           System.Environment
 
 data CasaPush =
@@ -97,23 +108,11 @@ statusCommand =
 populateCommand :: MonadIO m => PopulateConfig -> m ()
 populateCommand populateConfig =
   runPantryApp
-    (do let unresoledRawSnapshotLocation = populateConfigSnapshot populateConfig
-        rawSnapshotLocation <- resolvePaths Nothing unresoledRawSnapshotLocation
-        snapshotLocation <- completeSnapshotLocation rawSnapshotLocation
-        logSticky "Loading snapshot ..."
-        rawSnapshot <- loadSnapshot snapshotLocation
-        logStickyDone "Loaded snapshot."
-        let total = length (rsPackages rawSnapshot)
-        for_
-          (zip
-             [0 :: Int ..]
-             (map rspLocation (M.elems (rsPackages rawSnapshot))))
-          (\(i, rawPackageLocationImmutable) -> do
-             logSticky
-               ("Loading package: " <> display i <> "/" <> display total <> ": " <>
-                display rawPackageLocationImmutable)
-             loadPackageRaw rawPackageLocationImmutable)
-        logStickyDone ("Loaded all " <> display total <> " packages."))
+    (do rawSnapshot <-
+          loadSnapshotByUnresolvedSnapshotLocation unresoledRawSnapshotLocation
+        populateFromRawSnapshot rawSnapshot)
+  where
+    unresoledRawSnapshotLocation = populateConfigSnapshot populateConfig
 
 -- | Start pushing.
 pushCommand :: MonadIO m => PushConfig -> m ()
@@ -153,3 +152,70 @@ stickyProgress total = go (0 :: Int)
                   ("Pushing blobs: " <> display i' <> "/" <> display total)))
           yield v
           go i'
+
+-- | Download all snapshots from stackage. The results are
+-- paginated. We want everything, so we just keep increasing the page
+-- index until we get a null result.
+downloadAllSnapshotTextNames :: IO (Set Text)
+downloadAllSnapshotTextNames = go 1 mempty
+  where
+    go page acc = do
+      request <-
+        parseRequest ("https://www.stackage.org/snapshots?page=" ++ show page)
+      response :: Response Aeson.Value <- httpJSON request
+      case getResponseStatusCode response of
+        200 ->
+          case Aeson.parseEither snapshotsParser (getResponseBody response) of
+            Left err -> error err
+            Right snapshots -> loop (acc <> Set.fromList snapshots)
+              where loop =
+                      if null snapshots
+                        then pure
+                        else go (page + 1)
+        _ -> error ("Failed to download: " ++ show (getResponseStatus response))
+
+-- | Parse the JSON from Stackage.
+snapshotsParser :: Aeson.Value -> Aeson.Parser [Text]
+snapshotsParser j = do
+  o <- Aeson.parseJSON j
+  snapshots :: [[[Text]]] <- o Aeson..: "snapshots"
+  concatenatedSnapshots <-
+    fmap
+      concat
+      (mapM
+         (\grouping ->
+            mapM
+              (\snapshotSpec ->
+                 case listToMaybe snapshotSpec of
+                   Nothing -> fail "No snapshot name"
+                   Just name -> pure name)
+              grouping)
+         snapshots)
+  pure concatenatedSnapshots
+
+-- | Populate the database with packages from a raw snapshot.
+populateFromRawSnapshot ::
+     (HasLogFunc env, HasPantryConfig env, HasProcessContext env)
+  => RawSnapshot
+  -> RIO env ()
+populateFromRawSnapshot rawSnapshot = do
+  let total = length (rsPackages rawSnapshot)
+  for_
+    (zip [0 :: Int ..] (map rspLocation (M.elems (rsPackages rawSnapshot))))
+    (\(i, rawPackageLocationImmutable) -> do
+       logSticky
+         ("Loading package: " <> display i <> "/" <> display total <> ": " <>
+          display rawPackageLocationImmutable)
+       loadPackageRaw rawPackageLocationImmutable)
+  logStickyDone ("Loaded all " <> display total <> " packages.")
+
+-- | Load a snapshot by its unresolved raw snapshot location (the
+-- result of parsing from text).
+loadSnapshotByUnresolvedSnapshotLocation ::
+     (HasPantryConfig env, HasLogFunc env, HasProcessContext env)
+  => Unresolved RawSnapshotLocation
+  -> RIO env RawSnapshot
+loadSnapshotByUnresolvedSnapshotLocation unresoledRawSnapshotLocation = do
+  rawSnapshotLocation <- resolvePaths Nothing unresoledRawSnapshotLocation
+  snapshotLocation <- completeSnapshotLocation rawSnapshotLocation
+  loadSnapshot snapshotLocation
