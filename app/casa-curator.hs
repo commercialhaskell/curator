89109d2658bb1442531f48ed0b512cd1486a6681
Chris Done <chrisdone@gmail.com>
2019-11-04 16:14:27 +0000

    Add note about no new jobs

--- app/casa-curator.hs
+++ app/casa-curator.hs
@@ -443,22 +443,21 @@ pushCommand config = do
       (liftIO
          (withContinuousProcessDb (configSqliteFile config) (selectFirst [] [])))
   runPantryAppWith
-     (pushConfigConcurrentDownloads config)
-     (pullUrlString (pushConfigPullUrl config))
-     (pushConfigMaxBlobsPerRequest config)
+    (pushConfigConcurrentDownloads config)
+    (pullUrlString (pushConfigPullUrl config))
+    (pushConfigMaxBlobsPerRequest config)
     (runPantryStorage
-        (do count <- allBlobsCount mlastPushedBlobId
-            if count > 0
-              then blobsSink
-                     (pushUrlString (pushConfigPushUrl config))
-                     (allBlobsSource mlastPushedBlobId .|
-                      CL.mapM
-                        (\(blobId, blob) -> do
-                           liftIO
-                             (writeIORef mlastBlobIdRef (Just blobId))
-                           pure blob) .|
-                      stickyProgress count)
-              else pure ()))
+       (do count <- allBlobsCount mlastPushedBlobId
+           if count > 0
+             then blobsSink
+                    (pushUrlString (pushConfigPushUrl config))
+                    (allBlobsSource mlastPushedBlobId .|
+                     CL.mapM
+                       (\(blobId, blob) -> do
+                          liftIO (writeIORef mlastBlobIdRef (Just blobId))
+                          pure blob) .|
+                     stickyProgress count)
+             else logInfo "There are no new blobs to push since last time."))
   mlastBlobId <- liftIO (readIORef mlastBlobIdRef)
   case mlastBlobId of
     Nothing -> pure ()
