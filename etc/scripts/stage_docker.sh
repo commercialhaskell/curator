240c3766c6a84d042accc97dec87d0be8c14c833
Jose Duran <jose.daniel.duran.toro@gmail.com>
2020-03-17 04:16:05 -0500

    using bz2 for compression

--- etc/scripts/stage_docker.sh
+++ etc/scripts/stage_docker.sh
@@ -4,3 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 mkdir -p etc/docker/_artifacts
 stack build
 stack install --local-bin-path=etc/docker/_artifacts "$@"
+bzip2 -zk etc/docker/_artifacts/*
+mkdir -p etc/docker/_artifacts/bz2
+mv etc/docker/_artifacts/*.bz2 etc/docker/_artifacts/bz2
