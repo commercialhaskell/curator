02d0c2cfbd843072c097ec56173f6140d1d3b20e
Dan Burton <danburton.email@gmail.com>
2026-04-03 21:36:29 -0700

    mergeWithPreviousLtsBound: more careful version merge in case of intentional downgrade

--- src/Curator/ApplyPvpRules.hs
+++ src/Curator/ApplyPvpRules.hs
@@ -1,12 +1,22 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Curator.ApplyPvpRules (applyPvpRules) where
+module Curator.ApplyPvpRules
+  ( applyPvpRules
+  , mergeWithPreviousLtsBound
+  ) where
 
 import RIO
 import Pantry
 import Curator.Types
-import Distribution.Types.VersionRange (intersectVersionRanges, majorBoundVersion)
+import Distribution.Types.VersionRange (VersionRange, earlierVersion, intersectVersionRanges, majorUpperBound)
 import qualified RIO.Map as Map
+
+mergeWithPreviousLtsBound :: Version -> Maybe VersionRange -> VersionRange
+mergeWithPreviousLtsBound previousVersion mrange =
+  let inferred = earlierVersion (majorUpperBound previousVersion)
+   in case mrange of
+        Nothing -> inferred
+        Just range -> intersectVersionRanges inferred range
 
 -- | Apply the PVP rules to a set of constraints by adding in additional upper bounds.
 applyPvpRules
@@ -17,18 +27,20 @@ applyPvpRules
 applyPvpRules constraints0 major minor = do
   (snapshot, _, _) <- loadAndCompleteSnapshotRaw (defaultSnapshotLocation $ LTS major minor) mempty mempty
   let getBounds name =
-        case Map.lookup name $ snapshotPackages snapshot of
+        case Map.lookup name (snapshotPackages snapshot) of
           Nothing -> Nothing
-          Just sp -> Just $ majorBoundVersion $ packageLocationVersion $ spLocation sp
-  let goSource name (PSHackage hs) =
+          Just sp -> Just (packageLocationVersion (spLocation sp))
+
+      goSource name (PSHackage hs) =
         let range =
               case (getBounds name, hsRange hs) of
                 (Nothing, Nothing) -> Nothing
-                (Just x, Nothing) -> Just x
+                (Just x, Nothing) -> Just (mergeWithPreviousLtsBound x Nothing)
                 (Nothing, Just y) -> Just y
-                (Just x, Just y) -> Just $ intersectVersionRanges x y
+                (Just x, Just y) -> Just (mergeWithPreviousLtsBound x (Just y))
          in PSHackage hs { hsRange = range }
       goSource _name (PSUrl url) = PSUrl url
-  let goPC name pc = pc { pcSource = goSource name $ pcSource pc }
-  let packages = Map.mapWithKey goPC $ consPackages constraints0
+
+      goPC name pc = pc { pcSource = goSource name (pcSource pc) }
+      packages = Map.mapWithKey goPC (consPackages constraints0)
   pure constraints0 { consPackages = packages }
--- test/Curator/SnapshotSpec.hs
+++ test/Curator/SnapshotSpec.hs
@@ -1,16 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Curator.SnapshotSpec ( spec ) where
 
+import Curator (mergeWithPreviousLtsBound)
 import Curator.Snapshot
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Text (unpack)
 import Distribution.Types.PackageName (mkPackageName)
-import Distribution.Types.VersionRange (noVersion)
+import Distribution.Types.Version (mkVersion)
+import Distribution.Types.VersionRange (VersionRange, noVersion, withinRange)
+import Distribution.Text (simpleParse)
 import Test.Hspec
 
 spec :: Spec
 spec = do
+  describe "mergeWithPreviousLtsBound" $
+    it "allows downgrades within an explicit user range in LTS minor mode" $ do
+      let previous = mkVersion [0, 5, 5, 0]
+      let userRange =
+            simpleParse "^>= 0.5.4.0 && < 0.5.5.0"
+              :: Maybe VersionRange
+
+      case userRange of
+        Nothing -> expectationFailure "Failed to parse user range"
+        Just r -> do
+          let merged = mergeWithPreviousLtsBound previous (Just r)
+          withinRange (mkVersion [0, 5, 4, 1]) merged `shouldBe` True
+          withinRange (mkVersion [0, 5, 5, 0]) merged `shouldBe` False
+
   describe "pkgBoundsError" $
     context "when there is more than one github ping" $
       it "returns github pings separated by spaces" $ do
